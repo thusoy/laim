8b05dc06c10e576788c345545c42291418876773
Tarjei Husøy <git@thusoy.com>
2019-07-04 14:04:48 +0200

    Add skeleton

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import os
+import re
+from setuptools import setup, find_packages
+
+install_requires = []
+
+version_file = os.path.join(os.path.dirname(__file__), 'laim', '_version.py')
+with open(version_file) as fh:
+    version_file_contents = fh.read().strip()
+    version_match = re.match(r"__version__ = '(\d\.\d.\d.*)'", version_file_contents)
+    version = version_match.group(1)
+
+setup(
+    name='laim',
+    version=version,
+    author='Tarjei Husøy',
+    author_email='git@thusoy.com',
+    url='https://github.com/thusoy/laim',
+    description="Local SMTP helper",
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Web Environment',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Communications :: Email',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Systems Administration',
+    ],
+)
