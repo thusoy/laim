83379756a8b047f1dd83b9df489d5c6c90162e51
Tarjei Husøy <git@thusoy.com>
2019-07-07 13:35:29 +0200

    Refactor stdin patching
    Prefers a more specific mock to not overwrite sys.stderr and sys.exit.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,44 +9,44 @@ import pytest
 from laim.__main__ import read_message, main
 
 def test_read_message():
-    sys_mock = mock.Mock()
-    sys_mock.stdin.buffer = [
+    stdin_mock = mock.Mock()
+    stdin_mock.buffer = [
         b'From: foo\n',
         b'\n',
         b'Foo content\n',
         b'.\n',
         b'Should be ignored\n',
     ]
-    with mock.patch('laim.__main__.sys', sys_mock):
+    with mock.patch('laim.__main__.sys.stdin', stdin_mock):
         message = read_message('8BITMIME', stop_on_dot=True)
 
     assert message.get_payload() == 'Foo content\n'
 
 
 def test_read_message_ignore_dot():
-    sys_mock = mock.Mock()
-    sys_mock.stdin.buffer = [
+    stdin_mock = mock.Mock()
+    stdin_mock.buffer = [
         b'From: foo\n',
         b'\n',
         b'Foo content\n',
         b'.\n',
         b'Should be included\n',
     ]
-    with mock.patch('laim.__main__.sys', sys_mock):
+    with mock.patch('laim.__main__.sys.stdin', stdin_mock):
         message = read_message('8BITMIME', stop_on_dot=False)
 
     assert message.get_payload() == 'Foo content\n.\nShould be included\n'
 
 
 def test_message_parsing():
-    sys_mock = mock.Mock()
-    sys_mock.stdin.buffer = [
+    stdin_mock = mock.Mock()
+    stdin_mock.buffer = [
         b'From: foo\n',
         b'To: bar\n',
         b'\n',
         b'Message\n',
     ]
-    with mock.patch('laim.__main__.sys', sys_mock):
+    with mock.patch('laim.__main__.sys.stdin', stdin_mock):
         with mock.patch('laim.__main__.send_mail') as send_mail_mock:
             main([])
 
