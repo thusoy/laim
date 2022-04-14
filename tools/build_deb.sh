8bd593d1283c5aca0fb4baf95c408b9ed51f41cc
Tarjei Husøy <git@thusoy.com>
2022-04-14 16:38:34 +0200

    Build debs for bullseye

--- tools/build_deb.sh
+++ tools/build_deb.sh
@@ -32,7 +32,7 @@ get_source () {
 
 build_deb () {
     local container_id
-    for dist in buster; do
+    for dist in buster bullseye; do
         cd "$tempdir"
         sed "s/DISTRO/$dist/" Dockerfile-template > "Dockerfile-$dist"
         sudo docker build . -f "Dockerfile-$dist" -t "laim-$dist"
